ccf5d306fe87a111f4ea95c24ceaefd5091cc68d
VISHAL VAIRAL <42448155+thevishalvairal@users.noreply.github.com>
2020-08-25 11:20:29 +0530

    0001-SpringBoot-SpringData

---
+++ SpringDataProj1-CRUDRepo-Directmethod/src/main/java/com/nit/SpringDataProj1CrudRepoDirectmethodApplication.java
@@ -0,0 +1,12 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataProj1CrudRepoDirectmethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataProj1CrudRepoDirectmethodApplication.class, args);
+	}
+}
